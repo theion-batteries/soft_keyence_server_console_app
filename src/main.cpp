a54ff2b51174c9a0e484c72218216562c44175e5
sami-theion <sami@theion.de>
2023-02-23 13:49:59 +0100

    updated server

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,25 @@
 int main(int argc, char* argv[])
 {
     // Default value
-    std::string ip = "192.168.0.104";
+    std::string _ip = "192.168.0.104";
+    std::string ip = "";
+    // Prompt user for input
+    std::cout << "Enter an keyence controller ip, or press enter to use the default value: ";
+    std::getline(std::cin, ip);
 
     // Check if an argument was provided
     if (argc > 1) {
         ip = argv[1];
     }
 
+    // Use default value if input is empty
+    if (ip.empty()) {
+        ip = _ip;
+    }
     // Print out the argument
-    std::cout << "ip default: " << ip << std::endl;
+    std::cout << "ip used: " << ip << std::endl;
+    std::cout << "connect to this server forwarder with port: 6666 \n";
+    std::cout << "send th server ? to get sensor value \n";
 
     keyenceServer KS {ip};
     KS.run_server();
