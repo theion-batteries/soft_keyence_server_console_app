582b15b55964f65d6986798972e5ccea3562bdb7
sami-theion <sami@theion.de>
2023-02-27 17:25:09 +0100

    updated ip parser

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,21 @@ int main(int argc, char* argv[])
     // Default value
     std::string _ip = "192.168.0.104";
     std::string ip = "";
-    // Prompt user for input
-    std::cout << "Enter an keyence controller ip, or press enter to use the default value: ";
-    std::getline(std::cin, ip);
+
 
     // Check if an argument was provided
     if (argc > 1) {
+
         ip = argv[1];
     }
+    else
+    {
+    // Prompt user for input
+    std::cout << "Enter an keyence controller ip, or press enter to use the default value: ";
+    std::getline(std::cin, ip);
+    
+    }
+    
 
     // Use default value if input is empty
     if (ip.empty()) {
